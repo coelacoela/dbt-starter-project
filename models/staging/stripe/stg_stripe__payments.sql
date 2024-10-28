608525325c35e6e70b2f39bcf6bd0510c3a94433
coelacoela <sangho+github@nsh.nz>
2024-10-28 13:31:32 +1300

    Customers cleanups (#2)
    * Add stg_stripe__payments model
    
    * Revisit stg_strip__payments model
    
    * Add fct_orders model
    
    * Add lifetime_value to dim_customers model
    
    ---------
    
    Co-authored-by: coelacoela <sangho+dbt@nsh.nz>

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,9 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as status,
+    amount / 100 as amount, -- in dollars and cents
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
