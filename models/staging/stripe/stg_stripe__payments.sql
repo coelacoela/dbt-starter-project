c8d9b27d45f25fe4e0187030684871f77bcc9c51
coelacoela <sangho+dbt@nsh.nz>
2024-10-28 00:31:04 +0000

    Add lifetime_value to dim_customers model

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as status,
-    amount as amount,
-    created as created
+    amount / 100 as amount, -- in dollars and cents
+    created as created_at
 
 from {{ source('stripe', 'payment') }}
